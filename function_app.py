37b6de10fbc379e41214c0d16a859b30d11bebd2
ikuo <ikuo.pg@gmail.com>
2024-04-30 12:13:42 +0000

    debug

--- function_app.py
+++ function_app.py
@@ -7,7 +7,7 @@ import app as app
 fapp = func.FunctionApp()
 
 @fapp.route(route="http_trigger", auth_level=func.AuthLevel.ANONYMOUS)
-def main(req: func.HttpRequest) -> func.HttpResponse:
+def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     return func.WsgiMiddleware(app.app.wsgi_app).handle(req)
 
 #@app.route(route="http_trigger", auth_level=func.AuthLevel.ANONYMOUS)
