5b5f1ab27182bf7428747d12d21d61fe0c1d9c0e
ikuo <ikuo.pg@gmail.com>
2024-04-30 13:08:09 +0000

    debug

--- function_app.py
+++ function_app.py
@@ -2,13 +2,28 @@ import azure.functions as func
 import datetime
 import json
 import logging
-from app import app
 
-fapp = func.FunctionApp()
+app = func.FunctionApp()
 
-@fapp.route(route="http_trigger", auth_level=func.AuthLevel.ANONYMOUS)
+@app.route(route="http_trigger", auth_level=func.AuthLevel.ANONYMOUS)
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
-    return func.WsgiMiddleware(app.wsgi_app).handle(req)
+    logging.info('Python HTTP trigger function processed a request.')
 
-#@app.route(route="http_trigger", auth_level=func.AuthLevel.ANONYMOUS)
-#def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
+
+
