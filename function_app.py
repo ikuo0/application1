bb099db2d4f52aabaca301d1d03bf54a25a0c080
ikuo <ikuo.pg@gmail.com>
2024-04-30 12:16:09 +0000

    debug

--- function_app.py
+++ function_app.py
@@ -2,13 +2,13 @@ import azure.functions as func
 import datetime
 import json
 import logging
-import app as app
+from app import app
 
 fapp = func.FunctionApp()
 
 @fapp.route(route="http_trigger", auth_level=func.AuthLevel.ANONYMOUS)
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
-    return func.WsgiMiddleware(app.app.wsgi_app).handle(req)
+    return func.WsgiMiddleware(app.wsgi_app).handle(req)
 
 #@app.route(route="http_trigger", auth_level=func.AuthLevel.ANONYMOUS)
 #def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
